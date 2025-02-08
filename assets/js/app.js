41df20f2af582f6a62a4cde7d4baad0f1b5501fc
Daniel Galicia <mydevgalicia@gmail.com>
2025-02-08 17:07:25 -0600

    Windows system

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import motionSystem from './windows/motionSystem.js';
+import { controlsWindow } from './windows/controlsWindow.js';
+
+console.log('Importaciones correctas');
+
+motionSystem();
+controlsWindow(); 
---
+++ assets/js/windows/controlsWindow.js
@@ -0,0 +1,9 @@
+/* Funciones para las ventanas */
+
+export function controlsWindow() {
+    console.log('Controles de ventana');/* Esto es netamente de depuracion */
+
+    /* Algoritmo para cerrar la ventana */
+    
+
+}
